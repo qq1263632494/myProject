2de884b884289aa6089942f570be69a35547f889
qq1263632494 <1263632494@qq.com>
2019-09-16 13:20:43 +0800

    test1

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        User user = new User("admin", "男", 26);
+        session.insert("mapper.UserMapper.save", user);
+        session.commit();
+        session.close();
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,46 @@
+public class User {
+    private int id;
+    private String name;
+    private String sex;
+    private int age;
+    public User(){
+        super();
+    }
+    public User(String name, String sex, int age){
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
